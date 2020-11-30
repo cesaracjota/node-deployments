7a88fa2d6150aaa6452726fb1382d0962f3503e4
cesaracjota <acjotamerma@gmail.com>
2020-11-30 14:36:38 -0500

    subiendo la app

--- db.js
+++
@@ -1,9 +0,0 @@
-conn = new Mongo();
-db = conn.getDB("AppHeroku");
-
-db.Contactos.insert(
-  [
-   {Nombre: 'Jose', Apellidos:'Merma Condori', Correo:'jmerma@gmail.com', Celular: '942035890'},
-   {Nombre: 'Lourdes', Apellidos:'Castillo Lopez', Correo:'lcastillo@gmail.com', Celular: '942035890'},
-   {Nombre: 'Cesar', Apellidos:'Acjota Merma', Correo:'cacjota@gmail.com', Celular: '942035890'}
- ]);
